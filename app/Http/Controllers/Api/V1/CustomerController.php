9216b01cdfaa4d02046bdd23e3f4f4d3f734a135
kkamara <kamaracomputers@gmail.com>
2023-09-21 14:07:37 +0100

    Providing data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,14 +8,37 @@ use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use Illuminate\Http\Request;
+use App\Services\V1\CustomerQuery;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filter = new CustomerQuery();
+        $queryItems = $filter
+            ->transform($request);
+
+        if (count($queryItems) === 0) {
+            return new CustomerCollection(
+                Customer::paginate()
+            );
+        } else {
+            return new CustomerCollection(
+                Customer::where(
+                    $queryItems
+                )
+                    ->paginate()
+            );
+        }
+
+        Customer::where(
+            $queryItems
+        );
+
         return new CustomerCollection(
             Customer::paginate()
         );
